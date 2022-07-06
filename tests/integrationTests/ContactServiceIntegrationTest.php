ec9ccec6d03ed9f71c86409a564a82e9e90c448b
Alex-Decker <choudjaalex@gmail.com>
2022-07-06 16:43:41 +0200

    firty

--- tests/integrationTests/ContactServiceIntegrationTest.php
+++ tests/integrationTests/ContactServiceIntegrationTest.php
@@ -31,23 +31,50 @@ final class ContactServiceIntegrationTest extends TestCase
     // test de suppression de toute les données, nécessaire pour nettoyer la bdd de tests à la fin
     public function testDeleteAll()
     {
+        static::assertTrue($this->contact->deleteAll());
+        $data = $this->contact->getAllContacts();
+
     }
 
 
     public function testCreationContact()
     {
+        static::assertTrue($this->contact->createContact('testNom', 'testPrenom'));
+        $data = $this->contact->getAllContacts();
+        // echo "Creation contact :";
+        // echo var_dump($data);
+        static::assertSame('testNom', $data[0]['nom']);
+        static::assertSame('testPrenom', $data[0]['prenom']);
+        $this->id = $data[0]['id'];
     }
 
     public function testSearchContact()
     {
+        static::assertTrue($this->contact->searchContact('testNom'));
+        $data = $this->contact->getAllContacts();
+        // echo "Creation contact :";
+        // echo var_dump($data);
+        static::assertSame('testNom', $data[0]['nom']);
+        static::assertSame('testPrenom', $data[0]['prenom']);
+        $this->id = $data[0]['id'];
     }
 
     public function testModifyContact()
     {
+        static::assertTrue($this->contact->createContact('testNom', 'testPrenom'));
+        $data = $this->contact->updateContact(1,'toi','moi');
+        // echo "Creation contact :";
+        // echo var_dump($data);
+        static::assertSame('testNom', $data[0]['nom']);
+        static::assertSame('testPrenom', $data[0]['prenom']);
+        $this->id = $data[0]['id'];
     }
 
     public function testDeleteContact()
     {
+        static::assertTrue($this->contact->deleteContact(5));
+        $data = $this->contact->getAllContacts();
+
     }
 
 }
--- tests/unitTests/ContactServiceUnitTest.php
+++ tests/unitTests/ContactServiceUnitTest.php
@@ -26,22 +26,181 @@ final class ContactServiceUnitTest extends TestCase {
         parent::__construct($name, $data, $dataName);
         $this->contactService = new ContactService();
     }
-
+//Create
     public function testCreationContactWithoutAnyText() {
+        try {
+            $contactService = new ContactService();
+            $contactService->createContact('','');
+        }catch (\Exception $exception){
+            $this->assertStringContainsString('le nom doit être renseigné',$exception->getMessage());
+        }
     }
 
     public function testCreationContactWithoutPrenom() {
+        try {
+            $contactService = new ContactService();
+            $contactService->createContact('moi','');
+        }catch (\Exception $exception){
+            $this->assertStringContainsString('le prenom doit être renseigné',$exception->getMessage());
+        }
     }
 
     public function testCreationContactWithoutNom() {
+        try {
+            $contactService = new ContactService();
+            $contactService->createContact('moi','');
+        }catch (\Exception $exception){
+            $this->assertStringContainsString('le nom doit être renseigné',$exception->getMessage());
+        }
     }
-
+    public function testCreationContactWithNomPrenom() {
+        try {
+            $contactService = new ContactService();
+            $contactService->createContact('moi','toi');
+        }catch (\Exception $exception){
+        }
+    }
+//search
     public function testSearchContactWithNumber() {
+        try {
+            $contactService = new ContactService();
+            $contactService->searchContact(2);
+        }catch (\Exception $exception){
+            $this->assertStringContainsString('search doit être une chaine de caractères',$exception->getMessage());
+        }
     }
-
+    public function testSearchContactWithoutAnyText() {
+        try {
+            $contactService = new ContactService();
+            $contactService->searchContact();
+        }catch (\Exception $exception){
+            $this->assertStringContainsString('search doit être renseigné',$exception->getMessage());
+        }
+    }
+    public function testSearchContactWithText() {
+        try {
+            $contactService = new ContactService();
+            $contactService->searchContact('toi');
+        }catch (\Exception $exception){
+        }
+    }
+//modify
+    public function testModifyContactWithId() {
+        try {
+            $contactService = new ContactService();
+            $contactService->updateContact(1,'moi','toi');
+        }catch (\Exception $exception){
+        }
+    }
+    public function testModifyContactWithoutId() {
+        try {
+            $contactService = new ContactService();
+            $contactService->updateContact('','moi','toi');
+        }catch (\Exception $exception){
+            $this->assertStringContainsString("l'id doit être renseigné",$exception->getMessage());
+        }
+    }
+    public function testModifyContactWithoutPrenom() {
+        try {
+            $contactService = new ContactService();
+            $contactService->updateContact('','moi','');
+        }catch (\Exception $exception){
+            $this->assertStringContainsString("le prenom doit être renseigné",$exception->getMessage());
+        }
+    }
+    public function testModifyContactWithoutNom() {
+        try {
+            $contactService = new ContactService();
+            $contactService->updateContact(1,'','toi');
+        }catch (\Exception $exception){
+            $this->assertStringContainsString("le nom doit être renseigné",$exception->getMessage());
+        }
+    }
     public function testModifyContactWithInvalidId() {
+        try {
+            $contactService = new ContactService();
+            $contactService->updateContact(-5,'moi','toi');
+        }catch (\Exception $exception){
+            $this->assertStringContainsString("l'id doit être un entier non nul",$exception->getMessage());
+        }
     }
-
+//Delete
     public function testDeleteContactWithTextAsId() {
+        try {
+            $contactService = new ContactService();
+            $contactService->deleteContact('UN');
+        }catch (\Exception $exception){
+            $this->assertStringContainsString("l'id doit être renseigné",$exception->getMessage());
+        }
+    }
+    public function testDeleteContactEmptyId() {
+        try {
+            $contactService = new ContactService();
+            $contactService->deleteContact();
+        }catch (\Exception $exception){
+            $this->assertStringContainsString("l'id doit être un entier non nul",$exception->getMessage());
+        }
+    }
+    public function testDeleteContactIdNegatif() {
+        try {
+            $contactService = new ContactService();
+            $contactService->deleteContact(-8);
+        }catch (\Exception $exception){
+            $this->assertStringContainsString("l'id doit être un entier non nul",$exception->getMessage());
+        }
+    }
+    public function testDeleteContactWithvalidId() {
+        try {
+            $contactService = new ContactService();
+            $contactService->deleteContact(8);
+        }catch (\Exception $exception){
+        }
+    }
+    public function testDeleteAllContact() {
+        try {
+            $contactService = new ContactService();
+            $contactService->deleteAllContact();
+        }catch (\Exception $exception){
+        }
+    }
+//get
+    public function testGetContactWithvalidId() {
+        try {
+            $contactService = new ContactService();
+            $contactService->getContact("A");
+        }catch (\Exception $exception){
+        }
+    }
+    public function testGetContactWithTextAsId() {
+        try {
+            $contactService = new ContactService();
+            $contactService->getContact("A");
+        }catch (\Exception $exception){
+            $this->assertStringContainsString("l'id doit être un entier non nul",$exception->getMessage());
+        }
+    }
+    public function testGetContactWithInvalidId() {
+        try {
+            $contactService = new ContactService();
+            $contactService->getContact(null);
+        }catch (\Exception $exception){
+            $this->assertStringContainsString("l'id doit être un entier non nul",$exception->getMessage());
+        }
+    }
+    public function testGetContactWithEmptyId() {
+        try {
+            $contactService = new ContactService();
+            $contactService->getContact();
+        }catch (\Exception $exception){
+            $this->assertStringContainsString("l'id doit être renseigné",$exception->getMessage());
+        }
+    }
+    public function testGetAllContact() {
+        try {
+            $contactService = new ContactService();
+            $contactService->getAllContact();
+        }catch (\Exception $exception){
+
+        }
     }
 }
